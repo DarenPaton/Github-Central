40eb22fe44f29b74975077ca4dbe490611375ce1
Daren <daren@delliem.com>
2018-05-31 20:46:50 +0100

    added a test file

---
+++ Setup Time.ps1
@@ -0,0 +1,7 @@
+﻿#query time source
+w32tm /query /source
+
+#Set the NTP servers
+w32tm.exe /config /manualpeerlist:”0.uk.pool.ntp.org 1.uk.pool.ntp.org 2.uk.pool.ntp.org 3.uk.pool.ntp.org” /syncfromflags:manual /reliable:YES /update
+w32tm.exe /config /update
+Restart-Service w32time
